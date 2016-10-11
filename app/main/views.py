12fc966f8805cdc1f2732ae4f1da6832928c9f45
chenke91 <chenke91@qq.com>
2016-10-11 21:17:54 +0800

    add frontend

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,9 @@
 from . import main
 from flask import render_template
+from flask import jsonify
 
 @main.route('/login')
-def index():
+def login():
     return render_template('login.html')
 
 @main.route('/register')
@@ -12,3 +13,17 @@ def register():
 @main.route('/register/info')
 def register_info():
     return render_template('register_info.html')
+
+@main.route('/')
+def index():
+    site_url = 'http://127.0.0.1:5000'
+    return render_template('index.html', site_url=site_url)
+
+@main.route('/todo')
+def todo():
+    res = {
+        'code': 0,
+        'data': [],
+        'msg': None
+    }
+    return jsonify(res)
