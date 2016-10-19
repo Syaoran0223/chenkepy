96907675da0e1945adbd1d126e1e108386bf2c6a
zeus <12931421@qq.com>
2016-10-19 20:43:50 +0800

    user login

---
+++ app/main/forms.py
@@ -0,0 +1,9 @@
+from flask.ext.wtf import Form
+from wtforms import StringField,PasswordField,BooleanField,SubmitField
+from wtforms.validators import Required,Length
+
+class LoginForm(Form):
+    user_name = StringField('用户名',validators=[Required(), Length(1,64)])
+    password = PasswordField('密码',validators=[Required()])
+    remember_me = BooleanField('保持登录')
+    submit = SubmitField('登录')
--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,10 @@
+#coding: utf-8
+
 from flask import render_template, request, url_for, flash, redirect, session
-from flask.ext.login import login_user
+from flask.ext.login import login_user,logout_user,login_required
 
 from . import main
+from .forms import LoginForm
 from app.models import User, Region, School
 from app.exceptions import JsonOutputException
 
@@ -19,14 +22,16 @@ def register():
 
 @main.route('/login/', methods=['GET', 'POST'])
 def login():
-    # form = LoginFrom()
-    # if form.validate_on_submit():
-    #     user = User.query.filter_by(email=form.email.data).first()
-    #     if user is not None and user.verify_password(form.password.data):
-    #         login_user(user)
-    #         return redirect(request.args.get('next') or url_for('admin.get_blogs'))
-    #     flash('password error')
-    return render_template('login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+         user = User.query.filter_by(name=form.user_name.data).first()
+         if user is None:
+             user = User.query.filter_by(phone=form.user_name.data).first()
+         if user is not None and user.verify_password(form.password.data):
+             login_user(user)
+             return redirect(request.args.get('next') or url_for('main.index'))
+         flash('用户名或密码错误')
+    return render_template('login.html',form=form)
 
 @main.route('/register/info/')
 def register_info():
@@ -46,6 +51,48 @@ def register_info():
 def index():
     site_url = 'http://192.168.146.130:5000'
     return render_template('index.html', site_url=site_url)
+
+@login_required
+@main.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
+
+
+@amin.route('/reset', methods=['GET', 'POST'])
+def password_reset_request():
+    if not current_user.is_anonymous:
+        return redirect(url_for('main.index'))
+    form = PasswordResetRequestForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            token = user.generate_reset_token()
+            send_email(user.email, 'Reset Your Password',
+                       'auth/email/reset_password',
+                       user=user, token=token,
+                       next=request.args.get('next'))
+        flash('已发送密码重置的邮件至您注册时登记的邮箱中')
+        return redirect(url_for('main.login'))
+    return render_template('main/reset_password.html', form=form)
+
+
+@main.route('/reset/<token>', methods=['GET', 'POST'])
+def password_reset(token):
+    if not current_user.is_anonymous:
+        return redirect(url_for('main.index'))
+    form = PasswordResetForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is None:
+            return redirect(url_for('main.index'))
+        if user.reset_password(token, form.password.data):
+            flash('Your password has been updated.')
+            return redirect(url_for('auth.login'))
+        else:
+            return redirect(url_for('main.index'))
+    return render_template('auth/reset_password.html', form=form)
+
 
 @main.route('/todo')
 def todo():
