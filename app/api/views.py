8380d8421f89665e241e1604e10ef6f4d497b189
zeus <12931421@qq.com>
2016-11-30 19:13:19 +0800

    quest review

--- app/api/views.py
+++ app/api/views.py
@@ -11,7 +11,8 @@ from app.const import EXAM_STATUS,QUEST_IMAGE_STATUS
 from . import api_blueprint
 from app.models import Region, School, ExamReviewLog, Question, QuestReviewLog
 from app.sms import SmsServer
-from app.utils import render_api
+from app.utils import render_api,paginate
+from app import db
 
 import datetime
 @api_blueprint.route('/province')
@@ -547,12 +548,42 @@ def del_quest(id):
 
     return render_api('')
 
-#查看审核记录
+#查看预处理记录
 @api_blueprint.route('/paper/preprocess/log', methods=['GET'])
 def list_quest_review_log():
     pageIndex = int(request.args.get('pageIndex', 1))
     pageSize = int(request.args.get('pageSize', current_app.config['PER_PAGE']))
-    return QuestReviewLog.list_log(g.user.id, pageIndex, pageSize)
+
+
+    result = db.session.query(Question, QuestReviewLog, User).filter(Question.id == QuestReviewLog.exam_id,
+                                                                     QuestReviewLog.reviewer_id == g.user.id,
+                                                                     QuestReviewLog.review_state == EXAM_STATUS[
+                                                                         '预处理'] or QuestReviewLog.review_state ==
+                                                                     EXAM_STATUS[
+                                                                         '预处理完成'] and QuestReviewLog.reviewer_id == User.id).order_by(
+        QuestReviewLog.review_date.desc())
+    result = paginate(result, pageIndex, pageSize, error_out=False)
+    items = []
+    for item in result.items:
+        obj = {
+            'id': item.QuestReviewLog.id,
+            'quest_content': item.Question.quest_content_html,
+            'quest_no': item.Question.quest_no,
+            'reviewer': item.User.name,
+            'review_state': item.QuestReviewLog.review_state,
+            'review_date': item.QuestReviewLog.review_date.strftime("%Y-%m-%d %H:%M:%S"),
+            'review_memo': item.QuestReviewLog.review_memo
+        }
+        items.append(obj)
+
+    res = {
+        'items': items,
+        'pageIndex': result.page - 1,
+        'pageSize': result.per_page,
+        'totalCount': result.total,
+        'totalPage': result.pages
+    }
+    return res
 
 #试卷预处理完成
 @api_blueprint.route('/paper/preprocess/finish',methods=['POST'])
